1cfc8a4038ff8f84f5533335d90ea224eee5c7a6
Aidan Timson <aidan@timmo.dev>
2025-11-05 11:09:51 +0000

    Add missing and allow passing of aria attributes for dialogs

--- packages/webawesome/src/components/popover/popover.ts
+++ packages/webawesome/src/components/popover/popover.ts
@@ -2,6 +2,7 @@ import type { PropertyValues } from 'lit';
 import { html } from 'lit';
 import { customElement, property, query, state } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
 import { WaAfterHideEvent } from '../../events/after-hide.js';
 import { WaAfterShowEvent } from '../../events/after-show.js';
 import { WaHideEvent } from '../../events/hide.js';
@@ -87,6 +88,12 @@ export default class WaPopover extends WebAwesomeElement {
 
   /** Removes the arrow from the popover. */
   @property({ attribute: 'without-arrow', type: Boolean, reflect: true }) withoutArrow = false;
+
+  /** The ID of the element that labels the popover dialog. */
+  @property({ attribute: 'aria-labelledby' }) ariaLabelledby?: string;
+
+  /** The ID of the element that describes the popover dialog. */
+  @property({ attribute: 'aria-describedby' }) ariaDescribedby?: string;
 
   private eventController = new AbortController();
 
@@ -287,7 +294,12 @@ export default class WaPopover extends WebAwesomeElement {
 
   render() {
     return html`
-      <dialog part="dialog" class="dialog">
+      <dialog
+        aria-labelledby=${ifDefined(this.ariaLabelledby)}
+        aria-describedby=${ifDefined(this.ariaDescribedby)}
+        part="dialog"
+        class="dialog"
+      >
         <wa-popup
           part="popup"
           exportparts="
